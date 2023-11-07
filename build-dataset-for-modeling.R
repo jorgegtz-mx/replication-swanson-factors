f6020d2807d0f26fb35ef08bd9d2a09824830dc1
Jorge Gutierrez <jorge2gutierrez@gmail.com>
2023-11-07 09:53:18 -0600

    filling nulls in missing dates
    I was not calculating proxy changes for all National Holidays. Now I added an extra check of each data set. I verify if there is any NULL values after merging tables with the fed_factors table.e

--- build-dataset-for-modeling.R
+++ build-dataset-for-modeling.R
@@ -81,16 +81,18 @@ db2 <- db %>%
               select(Date, everything())) %>% 
   arrange(Date)
 
-# Now I will append the NAFTRAC prices. Use the alternative method for calculating the price change
-# on the Mexico's National Holidays.
+### Now I will append the NAFTRAC prices. Use the alternative method for calculating the price change on the Mexico's National Holidays.
+
+mexico_holidays2 <- sort(c(mexico_holidays, as.Date("2003-09-16"), as.Date("2006-12-12")))
+
 proxy_changes_naftrac <- naftrac %>% 
-  filter(Date %in% c((mexico_holidays - 1), (mexico_holidays + 1))) %>% 
+  filter(Date %in% c((mexico_holidays2 - 1), (mexico_holidays2 + 1))) %>% 
   arrange(Date) %>% 
   #Calculate the change between the last close price (the last business day before the holiday)
   #and the open price after the holiday.
   transmute(Date, naftrac_open, naftrac_close = lag(naftrac_close),
             log.diff.naftrac = log(naftrac_open) - log(naftrac_close)) %>% 
-  filter(Date %in% (mexico_holidays + 1)) %>% 
+  filter(Date %in% (mexico_holidays2 + 1)) %>% 
   mutate(Date = Date- 1) %>% 
   #we have data for sep-09-12, remove proxy calculation
   filter(Date != as.Date("2012-09-13"))
@@ -102,24 +104,27 @@ naftrac_clean <-  naftrac %>%
   arrange(Date) %>% 
   select(colnames(proxy_changes_naftrac))
 
+#Check if there is any announcement date that has a missing NAFTRAC data.
+fed_factors %>% 
+  left_join(naftrac_clean, by = 'Date') %>% 
+  filter(Date >= min(naftrac_clean$Date), is.na(naftrac_open))
+
 # Merge db2 with NAFTRAC prices
 db3 <- db2 %>% 
   left_join(naftrac_clean, by = "Date")
 
-
 ### Now I will append the treasury yield changes.
 proxy_changes_treasuries <- treasuries %>% 
-  filter(Date %in% c((mexico_holidays - 1), (mexico_holidays + 1))) %>% 
+  filter(Date %in% c((mexico_holidays2 - 1), (mexico_holidays2 + 1))) %>% 
   arrange(Date) %>%
   transmute(Date, dcetes28 = cetes28-lag(cetes28),
             dcetes91 = cetes91-lag(cetes91),
             dcetes182 = cetes182-lag(cetes182), 
             dcetes364 = cetes364-lag(cetes364)) %>% 
-  filter(Date %in% (mexico_holidays + 1)) %>% 
+  filter(Date %in% (mexico_holidays2 + 1)) %>% 
   mutate(Date = Date - 1) %>% 
   #we have data for sep-09-12, remove proxy calculation
   filter(Date != as.Date("2012-09-13"))
-
 
 # Bind all treasuries data with the proxy changees.
 # compute CETES one day yield change
@@ -132,16 +137,26 @@ treasuries_clean <- treasuries %>%
   filter(Date != "2003-05-16", !(Date %in% proxy_changes_treasuries$Date)) %>% 
   bind_rows(proxy_changes_treasuries)
 
+#Check if there is any announcement date that has a missing yield.
+fed_factors %>% 
+  left_join(treasuries_clean, by = 'Date') %>% 
+  filter(Date >= min(treasuries_clean$Date), is.na(dcetes28))
+
 # Merge db3 with table with changes of treasury yields.
 db4 <-db3 %>% 
   left_join(treasuries_clean, by = 'Date')
 
 ##Now I will append exchange rate changes
+
+#I need to add new dates to the "mexico_holidays" vector. Two of them are earlier than for the previous tables. I found data for the last two. It is weird, because they are holidays too but there was data for the NAFTRAC prices and Treasury yields.
+
+mexico_holidays3 <- sort(c(mexico_holidays2, as.Date("1997-02-05"), as.Date("2000-03-21")))
+
 proxy_exchange_rates_changes <- exchange_rates%>% 
-  filter(Date %in% c((mexico_holidays - 1), (mexico_holidays + 1))) %>% 
+  filter(Date %in% c((mexico_holidays3 - 1), (mexico_holidays3 + 1))) %>% 
   arrange(Date) %>% 
   mutate(log.diff.exchange.rate = log(lag(exchange.rate.close))-log(exchange.rate.open)) %>% 
-  filter(Date %in% (mexico_holidays + 1)) %>% 
+  filter(Date %in% (mexico_holidays3 + 1)) %>% 
   mutate(Date = Date - 1) %>% 
   #we have data for sep-09-12, remove proxy calculation
   filter(Date != as.Date("2012-09-13"))
@@ -153,22 +168,44 @@ exchange_rates_clean <- exchange_rates %>%
   filter(!(Date %in% proxy_exchange_rates_changes$Date)) %>% 
   bind_rows(proxy_exchange_rates_changes)
 
+#Check if there is any announcement date that has a exchange rate.
+fed_factors %>% 
+  left_join(exchange_rates_clean, by = 'Date') %>% 
+  filter(Date >= min(exchange_rates_clean$Date), is.na(log.diff.exchange.rate))
+
 # Merge fed factors table with changes on the exchange rate
 db5 <- fed_factors %>% 
-  inner_join(exchange_rates_clean, by = "Date") %>% 
-  #since the exchange rate series matches more dates in the fed_factors table, I will merge
-  #db4 into this
-  left_join(db4 %>% 
-              select(-c(ffr_shock, fw_shock, lsap_shock, lsap_shock2)), by = "Date")
+  filter(Date >= as.Date("1992-01-02")) %>% 
+  left_join(exchange_rates_clean, by = "Date")
+  
+#since the exchange rate series matches more dates in the fed_factors table, I will merge
+#db4 into db5
+db6 <- db5 %>% 
+  left_join(db4 %>% select(-c(ffr_shock, fw_shock, lsap_shock, lsap_shock2)), by = "Date")
 
 # Standardize date frequency to estimate IRF using Newey-West 
 # (Stata requires that time interval is constant)
 # I believe this should not change the results
-regular_monthly_frequency <- sort(map_vec(seq(1, nrow(db5), by = 1), 
+regular_monthly_frequency <- sort(map_vec(seq(1, nrow(db6), by = 1), 
                                           ~as.Date("2022-12-01")-months(.)))
 
-db5$Date.Regular.Freq <- regular_monthly_frequency
+db6$Date.Regular.Freq <- regular_monthly_frequency
 
-db6 <- db5 %>% select(Date, Date.Regular.Freq, everything())
+db7 <- db6 %>% select(Date, Date.Regular.Freq, everything())
 
-db6 %>% View()
+#Check that none date is duplicates
+sum(duplicated(db7$Date))
+
+#Save data set for modeling
+write_csv(db7, "data/dataset_modeling_swanson_mexico.csv")
+
+#Quality Checks
+#What date does the first obs. of each series matches in the fed_factors table?
+earliest_ipc_date <- (db7 %>% filter(!is.na(bmv.ipc.open)))$Date %>% min()
+earliest_naftrac_date <- (db7 %>% filter(!is.na(naftrac_open)))$Date %>% min()
+earliest_cetes_date <- (db7 %>% filter(!is.na(dcetes28)))$Date %>% min()
+earliest_exchangerate_date <- (db7 %>% filter(!is.na(exchange.rate.open)))$Date %>% min()
+c("earliest ipc date"=earliest_ipc_date, 
+  "earliest naftrac date"=earliest_naftrac_date,
+  "earliest cetes date"=earliest_cetes_date,
+  "earliest exchage rate date"=earliest_exchangerate_date)
